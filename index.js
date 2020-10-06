94491c6b39d028e76eda87a73d37ea93d63f4479
Divad666333 <davidpablo@protonmail.com>
2020-10-07 00:35:40 +0200

    commit

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ client.on('ready', () => {
 
 client.login(token);
 
-//TEST EDIT
-
 client.on('message', msg => {
 	if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 	const args = msg.content.slice(prefix.length).trim().split(/ +/);
